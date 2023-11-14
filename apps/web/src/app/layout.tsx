8a21dde28a7198fdf3db32bfac887a3530015bcc
Jarrian <glorianagojar@gmail.com>
2023-11-14 10:11:47 +0800

    remove shadcn ui

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
-import "./globals.css";
-import "ui/src/styles/globals.css";
+import "./globals.scss";
+import "ui/styles/globals.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -2,17 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
 	content: [
+		"./**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./lib/**/*.{js,ts,jsx,tsx,mdx}",
-		"./styles/**/*.{js,ts,jsx,tsx,mdx}",
+		"./providers/**/*.{js,ts,jsx,tsx,mdx}",
 		"../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	darkMode: ["class"],
 	theme: {
-		extend: {
-		},
+		extend: {},
 	},
+	darkMode: "class",
 	plugins: [],
 };
 export default config;
