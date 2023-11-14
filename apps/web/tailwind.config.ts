bd53eadf489b55126656cf77a398d798c9b80421
Jarrian <glorianagojar@gmail.com>
2023-11-14 10:17:20 +0800

    fix: import with ts extensions

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,7 @@
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-import uiConfig from "ui/tailwind.config.ts";
+import uiConfig from "ui/tailwind.config";
 
 const config: Pick<Config, "presets"> = {
   presets: [uiConfig],
