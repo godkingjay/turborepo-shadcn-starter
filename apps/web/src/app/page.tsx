d284a47585cd39d2640cd4d3e45e7b0852580db7
Jarrian <glorianagojar@gmail.com>
2023-11-14 09:40:44 +0800

    deps: use yarn

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "ui"
-
 export default function Page(): JSX.Element {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="flex flex-col gap-y-2">
-				<Button>
+				<button>
 					Web Application
-				</Button>
+				</button>
 			</div>
 		</main>
 	);
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,7 +1,1 @@
 import "./styles/globals.css"
-
-// components
-export * from "./components/ui/button"
-
-// lib
-export * from "./lib/utils"
